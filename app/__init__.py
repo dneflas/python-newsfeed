9a919b144a83c73aa7eafa913ca9f7d396e60820
Debbie Neflas <debbieneflas@gmail.com>
2022-11-22 12:13:44 -0800

    render homepage and single page with db query

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .routes import home, dashboard
-
+from app.db import init_db
 
 def create_app(test_config=None):
   # set up app config
@@ -16,5 +16,7 @@ def create_app(test_config=None):
 
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+
+  init_db(app)
 
   return app
