1a9b3e574ce88d09f473852225fcbf869c07a8dd
Debbie Neflas <debbieneflas@gmail.com>
2022-12-20 14:10:53 -0800

    add auth to dashboard and api routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, session
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -60,6 +61,7 @@ def login():
     return jsonify(id = user.id)
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
     data = request.get_json()
     db = get_db()
@@ -83,6 +85,7 @@ def comment():
     return jsonify(id = newComment.id)
 
 @bp.route('posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
     data = request.get_json()
     db = get_db()
@@ -104,6 +107,7 @@ def upvote():
     return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
     data = request.get_json()
     db = get_db()
@@ -126,6 +130,7 @@ def create():
     return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
     data = request.get_json()
     db = get_db()
@@ -143,6 +148,7 @@ def update(id):
     return '', 204
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
     db = get_db()
 
